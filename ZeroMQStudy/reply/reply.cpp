3740f5b572549c381730b26c8cf5d22960043def
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2025-02-22 16:19:05 +0900

    1. request_reply 패턴 업데이트

---
+++ ZeroMQStudy/reply/reply.cpp
@@ -0,0 +1,34 @@
+﻿// reply.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#include <zmq.hpp>
+#include <Windows.h>
+#include <string>
+
+#define sleep(n)	Sleep(n)
+
+int main()
+{
+    // 소켓 컨텍스트 준비
+    zmq::context_t context(2);
+    zmq::socket_t socket(context, zmq::socket_type::rep);
+    socket.bind("tcp://*:5555");
+
+    while (true) {
+        zmq::message_t request;
+
+        //  클라이언트 응답 기다림
+        socket.recv(request, zmq::recv_flags::none);
+        std::cout << "Received Hello" << std::endl;
+
+        //  작업
+        sleep(1);
+
+        // 클라이언트로 응답
+        std::string temp = "world!!";
+        zmq::message_t reply(temp);
+        socket.send(reply, zmq::send_flags::none);
+    }
+}
---
+++ ZeroMQStudy/request/request.cpp
@@ -0,0 +1,33 @@
+﻿// request.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#include <zmq.hpp>
+
+int main()
+{
+    //  Context 및 소켓 준비
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, zmq::socket_type::req);
+
+    std::cout << "Connecting to hello world server..." << std::endl;
+    socket.connect("tcp://localhost:5555");
+
+    // 10번 반복
+    for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
+        std::string request_context = "Hello!!!";
+        zmq::message_t request(request_context);
+        std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
+        auto send_result = socket.send(request, zmq::send_flags::none);
+     
+        
+        //  Get the reply.
+        zmq::message_t reply;
+        auto result = socket.recv(reply, zmq::recv_flags::none);
+        std::cout << "Received World " << request_nbr << std::endl;
+
+
+    }
+}
+
